c20b5498fcdf7c235c8fae85c5614e1338918dca
ddaynew365 <ddaynew365@gmail.com>
2021-12-22 18:17:13 +0900

    JS:prototype

--- JS/practice.js
+++ JS/practice.js
@@ -1,5 +1,11 @@
-let a
-{
-  console.log(a);
-  // let a =4;
-}
+// const a = 3;
+// var b = 3;
+// this.a = 2;
+// console.log(a);
+// console.log(b);
+
+// console.log(global.a);
+// console.log(this.b);
+// console.log(this.a);
+// console.log(this===global);
+
