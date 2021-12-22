430f3fb4c1397caa8eebf204b3fc6982802c5df1
ddaynew365 <ddaynew365@gmail.com>
2021-12-22 22:42:59 +0900

    JS: prototype 초안

--- JS/practice.js
+++ JS/practice.js
@@ -1,11 +1,1 @@
-// const a = 3;
-// var b = 3;
-// this.a = 2;
-// console.log(a);
-// console.log(b);
-
-// console.log(global.a);
-// console.log(this.b);
-// console.log(this.a);
-// console.log(this===global);
 
